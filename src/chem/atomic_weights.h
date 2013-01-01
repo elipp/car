4a55266fdfdde383728915767609a8446e5bb739
elipp <eliaspmiettinen@gmail.com>
2013-01-01 21:58:43 +0200

    lolz

--- src/chem/atomic_weights.h
+++
@@ -1,7 +0,0 @@
-#ifndef ATOMIC_WEIGHTS_H
-#define ATOMIC_WEIGHTS_H
-
-#include "../definitions.h"
-extern const key_constant_pair atomic_weight_table[];
-extern const size_t atomic_weight_table_size;
-#endif
